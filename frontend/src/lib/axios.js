db10bd15e892ae02dd0ab13744b42e705eba0e25
sadhef <muhammedsadhef@gmail.com>
2025-04-28 02:00:18 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL: (() => {
+    if (import.meta.env.MODE === "development") {
+      return "http://localhost:5000/api";
+    } else if (window.location.origin.includes("vercel.app")) {
+      return "https://ecommerce-u1pz.vercel.app/api";
+    } else {
+      return "/api";
+    }
+  })(),
+  withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
