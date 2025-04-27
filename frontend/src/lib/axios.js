4e4204109ef242ae7f9c49b561b63f2db99ce555
sadhef <muhammedsadhef@gmail.com>
2025-04-28 02:08:18 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: (() => {
-    if (import.meta.env.MODE === "development") {
-      return "http://localhost:5000/api";
-    } else if (window.location.origin.includes("vercel.app")) {
-      return "https://ecommerce-u1pz.vercel.app/api";
-    } else {
-      return "/api";
-    }
-  })(),
-  withCredentials: true, // send cookies to the server
+	baseURL: import.meta.mode === "development" ? "https://ricarts.vercel.app/" : "/api",
+	withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
