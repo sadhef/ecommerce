ffc4ce19ad2df1b8894c82f1d662e6c445ee153c
sadhef <muhammedsadhef@gmail.com>
2025-04-28 19:29:23 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://ecommerce-h3q3.vercel.app/" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
